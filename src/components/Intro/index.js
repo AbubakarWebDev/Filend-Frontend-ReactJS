45efdbf805e71b7b54f7b36053acb7d35ec35540
samar-almas <samaralmas8@gmail.com>
2023-07-30 08:54:57 +0500

    small routing issue fixed

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -5,16 +5,19 @@ const Intro = ({ receiver }) => {
     <div className="max-w-lg px-5 lg:mb-0">
       <div className="max-w-xl py-5">
         <h2 className="max-w-lg mb-6 text-3xl font-bold text-gray sm:text-4xl sm:leading-none ">
-          {receiver ? "Receiving Your File With Our Filend Application" : "Share files directly from your device to anywhere"}
+          {receiver
+            ? "Receiving Your File With Our Filend Application"
+            : "Share files directly from your device to anywhere"}
         </h2>
 
-        <p className="text-base text-gray-font md:text-lg mb-6"> 
-          Send files of any size directly from your device without ever storing anything online.
+        <p className="text-base text-gray-font md:text-lg mb-6">
+          Send files of any size directly from your device without ever storing
+          anything online.
         </p>
-        
+
         <div className="flex">
           <NavLink
-            to={"/register"}
+            to={"/signup"}
             className="mt-2 p-3 text-base font-bold rounded-md bg-[#E18A07] hover:bg-btn-secondry text-white"
           >
             Register Here
