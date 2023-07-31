5a55387f2a5355511e9ebe85f06151460cc6c185
Muhammad Abubakar <90tricks90@gmail.com>
2023-08-01 03:20:04 +0500

    Fixing UI of benefits section

--- src/components/Benifits/index.js
+++ src/components/Benifits/index.js
@@ -1,11 +1,17 @@
+import waveImg from "../../assets/images/wave-large-reversed.png";
+
 const Benifits = () => {
   return (
-    <section className="bg-transparent h-[200px] py-5 md:h-[150px] md:py-10 mb-20">
-      <div className="container mx-auto text-center">
-        <h2 className="text-gray mb-2 text-[35px] font-bold md:mb-4 md:text-[40px]">
+    <section 
+      style={{ background: "linear-gradient(to right, #a517ba, #5f1782)" }} 
+      className="min-h-[400px]"
+    >
+      <img src={waveImg} alt="Image" className="w-full" />
+      <div className="text-center flex flex-col justify-center items-center min-h-[250px] w-full">
+        <h2 className="text-white mb-2 text-[35px] font-bold md:mb-4 md:text-[40px]">
           Free! Secure! Unlimited! Open Source.
         </h2>
-        <p className="text-xl text-gray-font font-normal">
+        <p className="text-xl text-white font-normal">
           Send or Receive files without worrying about limits or charges.
         </p>
       </div>
